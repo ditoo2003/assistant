9e76036c260a3fc36c56fae9bee928695c649436
ditoo2003 <you@example.comditoo@qq.com>
2020-10-29 18:20:22 +0800

    第一次发布的版本

---
+++ ops/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# -*-encoding=utf8 -*-
+
+
+import logging
+
+
+class TestFilter(logging.Filter):
+
+    def filter(self, record):
+        if '[FILTER FLAG]' in record.msg:
+            return False
+        return True
+
