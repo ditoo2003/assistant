053d78b0d454ffa0d154e948be9dfa2d4ffdc8db
ditoo <ditoo@qq.com>
2020-10-30 20:13:37 +0800

    modify mail send

--- ops/maildemo.py
+++ ops/maildemo.py
@@ -5,13 +5,13 @@
 import os
 import django
 import smtplib
-from backend import settings
+# from backend import settings
 from email.mime.text import MIMEText
 
 from django.core import mail
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
-django.setup()
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+# django.setup()
 
 
 def send_mail():
@@ -19,19 +19,32 @@ def send_mail():
     content = 'hello django mail'
     receivers = ['ditoo2003@163.com']
     status = mail.send_mail(subject=title, message=content,
-                            from_email=settings.EMAIL_FROM, recipient_list=receivers)
+                            from_email='ditoo2003@qq.com', recipient_list=receivers)
     print(status)
 
-def send_mail_native():
+# def send_mail_native():
+#     msg = MIMEText("邮件通道测试", "plain", 'utf-8')
+#     msg['FROM'] = "Mail Test"
+#     msg['Subject'] = "【Mail Test】"
+#     receivers = ['ditoo2003@163.com']
+#     server = smtplib.SMTP_SSL(settings.EMAIL_HOST, settings.EMAIL_PORT)
+#     server.set_debuglevel(1)
+#     server.login(settings.EMAIL_HOST_USER, settings.EMAIL_HOST_PASSWORD)
+#     server.sendmail(settings.EMAIL_FROM, receivers, msg.as_string())
+#     server.close()
+
+
+def send_mail_test():
     msg = MIMEText("邮件通道测试", "plain", 'utf-8')
     msg['FROM'] = "Mail Test"
     msg['Subject'] = "【Mail Test】"
     receivers = ['ditoo2003@163.com']
-    server = smtplib.SMTP_SSL(settings.EMAIL_HOST, settings.EMAIL_PORT)
+    server = smtplib.SMTP_SSL('smtp.qq.com', 465)
     server.set_debuglevel(1)
-    server.login(settings.EMAIL_HOST_USER, settings.EMAIL_HOST_PASSWORD)
-    server.sendmail(settings.EMAIL_FROM, receivers, msg.as_string())
+    server.login('ditoo2003@qq.com', 'ltcvswlgjkbyhgda')
+    server.sendmail('ditoo2003@qq.com', receivers, msg.as_string())
     server.close()
 
+
 if __name__ == '__main__':
-    send_mail_native()
+    send_mail_test()
